bf376ccc6dfd300391b358f102d7ee11571f1ca9
NIKHILL0393 <393.nikhil@gmail.com>
2024-04-12 23:11:22 +0530

    complete

---
+++ JS/dom/targeting methods/dom-main.js
@@ -0,0 +1,11 @@
+var element;
+// element=document.URL;
+// element=document;
+// element=document.all[6];
+// element=document.title;
+// element=document.body;
+// element=document.links;
+// element=document.images;
+element=document.getElementsByClassName("a");
+  
+console.log(element);
